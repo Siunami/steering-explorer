315d4fcb6f4d4f830ad51dc73d466c10a0115818
siunami <siunami.matt@gmail.com>
2024-07-17 01:08:56 -0700

    revert to what it was

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const getBaseUrl = () => {
 	return process.env.NODE_ENV === "development"
-		? "http://localhost:5000/get_data?index="
-		: "https://siunami--example-get-started-webapp-get-data-dev.modal.run?index=";
+		? "http://localhost:5000"
+		: "https://steering-explorer-server.vercel.app";
 };
 
 export const fetchData = async (index) => {
